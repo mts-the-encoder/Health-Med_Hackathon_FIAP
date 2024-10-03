1c13bcf85fc29919231c5416ba565f740c8a53b9
mts-the-encoder <matheus.nsantos@boavistascpc.com.br>
2024-10-03 11:37:00 -0300

    paciente - validations tests

---
+++ src/tests/CommonTestUtilities/Requests/PacienteRequestBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Bogus;
+using Bogus.Extensions.Brazil;
+using Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class PacienteRequestBuilder
+{
+	public static CreatePacienteRequest Build()
+	{
+		return new Faker<CreatePacienteRequest>()
+			.RuleFor(x => x.Nome, f => f.Person.FullName)
+			.RuleFor(x => x.Email, f => f.Internet.Email())
+			.RuleFor(x => x.CPF, f => f.Person.Cpf())
+			.RuleFor(x => x.Senha, f => f.Internet.Password());
+	}
+}
---
+++ src/tests/Validators.Tests/Paciente/CreatePacienteValidatorTest.cs
@@ -0,0 +1,88 @@
+﻿using Application.UseCases.Paciente.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Paciente;
+
+public class CreatePacienteValidatorTest
+{
+	[Fact]
+	public void Success()
+	{
+		//Arrange
+		var validator = new RegisterPacienteValidator();
+		var request = PacienteRequestBuilder.Build();
+
+		//Act
+		var result = validator.Validate(request);
+
+		//Assert
+		result.IsValid.Should().BeTrue();
+	}
+
+	[Theory]
+	[InlineData("")]
+	[InlineData("      ")]
+	[InlineData(null)]
+	public void Error_Name_Empty(string name)
+	{
+		var validator = new RegisterPacienteValidator();
+		var request = PacienteRequestBuilder.Build();
+		request.Nome = name;
+
+		var result = validator.Validate(request);
+
+		result.IsValid.Should().BeFalse();
+		result.Errors.Should()
+			.ContainSingle()
+				.And
+			.Contain(e => e.ErrorMessage.Equals("Nome não pode ser vazio"));
+	}
+
+	[Theory]
+	[InlineData("")]
+	[InlineData("      ")]
+	[InlineData(null)]
+	public void Error_Email_Empty(string email)
+	{
+		var validator = new RegisterPacienteValidator();
+		var request = PacienteRequestBuilder.Build();
+		request.Email = email;
+
+		var result = validator.Validate(request);
+
+		result.IsValid.Should().BeFalse();
+		result.Errors.Should()
+			.ContainSingle()
+				.And
+			.Contain(e => e.ErrorMessage.Equals("Email não pode ser vazio"));
+	}
+
+	[Fact]
+	public void Error_Email_Invalid()
+	{
+		var validator = new RegisterPacienteValidator();
+		var request = PacienteRequestBuilder.Build();
+		request.Email = "welisson.com";
+
+		var result = validator.Validate(request);
+
+		result.IsValid.Should().BeFalse();
+		result.Errors.Should()
+			.ContainSingle()
+				.And
+			.Contain(e => e.ErrorMessage.Equals("Email inválido"));
+	}
+
+	[Fact]
+	public void Error_Password_Empty()
+	{
+		var validator = new RegisterPacienteValidator();
+		var request = PacienteRequestBuilder.Build();
+		request.Senha = string.Empty;
+
+		var result = validator.Validate(request);
+
+		result.IsValid.Should().BeFalse();
+	}
+}
